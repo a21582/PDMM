0127d30c042413f1abb057530fa6c8ad90d256c0
Marcelo Carvalho <a21582@alunos.ipca.pt>
2021-04-13 22:04:30 +0100

    Transport data between activies

--- HelloWorld/app/src/main/java/com/example/class001/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/class001/MainActivity.kt
@@ -1,18 +1,39 @@
 package com.example.class001
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val email = this.findViewById<EditText>(R.id.editTextTextEmailAddress)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+         super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==1)
+        {
+            if (resultCode == Activity.RESULT_OK)
+            {
+                val email = this.findViewById<EditText>(R.id.editTextTextEmailAddress)
+                email.setText(data?.getStringExtra("returnEmail"))
+            }
+        }
     }
 
     fun Teste_fun(view: View) {
         val intent = Intent(this@MainActivity, MainLogin::class.java )
-        startActivity(intent)
+        val email = this.findViewById<EditText>(R.id.editTextTextEmailAddress)
+        intent.putExtra("email", email.text.toString())
+        startActivityForResult(intent, 1)
     }
 }
--- HelloWorld/app/src/main/java/com/example/class001/MainLogin.kt
+++ HelloWorld/app/src/main/java/com/example/class001/MainLogin.kt
@@ -1,16 +1,28 @@
 package com.example.class001
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_login)
+
+        val main_email = intent.getStringExtra("email")
+        val get_email = this.findViewById<EditText>(R.id.reciveEmailText)
+        get_email.setText(main_email)
     }
 
     fun btnCloseLogin(view: View) {
+        val returnItentent = Intent()
+        val get_email = this.findViewById<EditText>(R.id.reciveEmailText)
+        returnItentent.putExtra("returnEmail", get_email.text.toString())
+        setResult(Activity.RESULT_OK,  returnItentent)
         finish()
     }
 }
