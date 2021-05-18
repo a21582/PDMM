0a75bfc093ec431addd9fba1f3d8febc3309f5f5
pedrocalado <pcalado99@gmail.com>
2021-05-18 21:04:35 +0100

    List sub-menu page "More"

---
+++ app/src/main/java/com/ipca/formulaworld/MenuOption.kt
@@ -0,0 +1,8 @@
+package com.ipca.formulaworld
+
+import android.media.Image
+
+class MenuOption(title: String, image: Int) {
+    val title: String = title
+    val image: Int = image
+}
--- app/src/main/java/com/ipca/formulaworld/ui/more/MoreFragment.kt
+++ app/src/main/java/com/ipca/formulaworld/ui/more/MoreFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import com.ipca.formulaworld.R
+import com.ipca.formulaworld.MenuOption
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,5 +27,24 @@ class MoreFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_more, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        val recyclerView = view.findViewById<RecyclerView>(R.id.main_recyclerview)
+
+        val listView = view.findViewById<ListView>(R.id.more_listview)
+
+        val options = mutableListOf<MenuOption>(
+                MenuOption("Estatísticas", R.drawable.ic_statistics),
+                MenuOption("História", R.drawable.ic_history),
+                MenuOption("Fórum", R.drawable.ic_discussion),
+        )
+        val adapter = MoreArrayAdapter(view.context, options)
+        listView.adapter = adapter
+
+//        recyclerView.adapter = MoreAdapterRec(listOf(MenuOption("Teste1", android.R.drawable.star_on)))
+//        recyclerView.layoutManager = LinearLayoutManager(view.context)
     }
 }
