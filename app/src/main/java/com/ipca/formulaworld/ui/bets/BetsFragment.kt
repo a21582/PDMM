0f07ca4a47b25f6dba452acf454432356ab8bfcc
pedrocalado <pcalado99@gmail.com>
2021-05-25 21:36:24 +0100

    Sign In Activity and Google Sing in

--- app/src/main/java/com/ipca/formulaworld/ui/bets/BetsFragment.kt
+++ app/src/main/java/com/ipca/formulaworld/ui/bets/BetsFragment.kt
@@ -19,10 +19,7 @@ private const val ARG_PARAM2 = "param2"
  */
 class BetsFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bets, container, false)
     }
