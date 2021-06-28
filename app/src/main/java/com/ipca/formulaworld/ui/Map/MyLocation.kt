5385cd902601d676f35f712d3e69895c92c11568
adrianojloureiro <adriano.loureiro99@gmail.com>
2021-06-29 00:06:42 +0100

    Bug fix location

--- app/src/main/java/com/ipca/formulaworld/ui/Map/MyLocation.kt
+++ app/src/main/java/com/ipca/formulaworld/ui/Map/MyLocation.kt
@@ -1,29 +1,37 @@
 package com.ipca.formulaworld.ui.Map
 
 import android.Manifest
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.ipca.formulaworld.R
+import com.ipca.formulaworld.ui.bets.BetsCompetitionFragment
+import com.ipca.formulaworld.ui.more.MoreFragment
 
 
 class MyLocation : AppCompatActivity(), OnMapReadyCallback {
 
 
-    lateinit var currentLocation : Location
+
     private lateinit var map:GoogleMap
     private val LOCATION_PERMISSION_REQUEST = 1
 
@@ -62,6 +70,7 @@ class MyLocation : AppCompatActivity(), OnMapReadyCallback {
                 map.isMyLocationEnabled = true
 
 
+
             }
             else {
                 Toast.makeText(this, "User has not granted location access permission", Toast.LENGTH_LONG).show()
@@ -82,47 +91,21 @@ class MyLocation : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
 
+
+
     }
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         getLocationAccess()
-        val lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-         if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-        val location =    lm.getLastKnownLocation(LocationManager.GPS_PROVIDER)
 
-        val longitude = location!!.longitude
-        val latitude = location.latitude
 
-        val position = LatLng(latitude, longitude)
-        // zoomLevel is set at 10 for a City-level view
-        val zoomLevel = 16f
-        // latLng contains the coordinates where the marker is added
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(position,zoomLevel))
-
-        //val latLng = LatLng(currentLocation!!.latitude, currentLocation!!.longitude)
-        //val markerOptions = MarkerOptions().position(latLng).title("I Am Here!")
-        //map.addMarker(markerOptions)
-        //map.animateCamera(CameraUpdateFactory.newLatLng(latLng))
-        //map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15f))
+        
 
     }
 
+    fun close(view: View) {
 
+        finish()
+    }
 }
 
