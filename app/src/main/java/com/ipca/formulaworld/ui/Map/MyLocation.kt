7ea19a2b97098db6d2e88ff138a0f790dd4ae5f0
adrianojloureiro <adriano.loureiro99@gmail.com>
2021-06-28 21:58:38 +0100

    Update Mylocation permission

--- app/src/main/java/com/ipca/formulaworld/ui/Map/MyLocation.kt
+++ app/src/main/java/com/ipca/formulaworld/ui/Map/MyLocation.kt
@@ -87,7 +87,25 @@ class MyLocation : AppCompatActivity(), OnMapReadyCallback {
         map = googleMap
         getLocationAccess()
         val lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        val location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+         if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        val location =    lm.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+
         val longitude = location!!.longitude
         val latitude = location.latitude
 
